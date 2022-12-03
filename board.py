381fbcaea02b01c3e779088ea7940c40292a25ab
Marvin Howell <m.howellaguirre@ufl.edu>
2022-12-03 13:10:56 -0500

    Board

---
+++ board.py
@@ -0,0 +1,21 @@
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+
+    def select(self, row, col):
+
+
+    def clear(self):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def check_board(self):
+        pass
