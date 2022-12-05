0d0984ed337091c501a7e13ca71ae889bd8a8a82
Ba2ix <d.ponto@ufl.edu>
2022-12-04 20:13:13 -0500

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+from sudoku_generator import *
 
 program_running = True
 sudoku_difficulty = 0
@@ -26,11 +27,18 @@ while program_running:
         print("You can choose 'edit' to edit the board or 'reset' to reset to board.")
         print("You can also type 'restart' to return to the difficulty select or 'exit' to exit the program.")
         full_board = generate_sudoku(9, 0)
+        #sudoku_board = (function)
         sudoku_running = True
         while sudoku_running:
-            
-            #get and print board
-            
+            index_row = 0
+            for row in sudoku_board: #print board with single digit and spaced out by box
+                for index_column in range(0,len(row)):
+                    print(f"{row[index_column]}", end=" ")
+                    if (index_column+1)%3 == 0 and index_column != 0: #third number double space
+                        print(end=" ")
+                print("")
+                if (index_row+1)%3 == 0 and index_row != 0: #third column space
+                    print("")
             user_inpt = input("What do you want to do? ")
             if user_inpt == 'edit':
                 user_row = int(input("Enter the row from the top you want to edit. "))
